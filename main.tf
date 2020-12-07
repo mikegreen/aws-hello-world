d4721edb698a98e5e70732e317770340d97e822e
Mike Green <772413+mikegreen@users.noreply.github.com>
2020-12-07 09:58:35 -0700

    init commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    organization = "hashicorp"
+
+    workspaces {
+      name = "aws-hello-world"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "ec2-example" {
+  instance_type = "t2.micro"
+}
