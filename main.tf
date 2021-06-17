79568d284dfa791ebf10afbb637690a97c2d357e
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-06-17 12:36:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,15 +18,15 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
-    # default_test_thing {
-    #   # This tests if a new, unknown tag appears will it bark
-    #   foo="foo"
-    # }
+  # default_test_thing {
+  #   # This tests if a new, unknown tag appears will it bark
+  #   foo="foo"
+  # }
 
-    default_tags {
+  default_tags {
     tags = {
       tag01 = "default_tag01"
-      tag02       = "Ops"
+      tag02 = "Ops"
     }
   }
 
@@ -36,8 +36,10 @@ resource "aws_instance" "ec2-example" {
   instance_type = "t2.micro"
   ami           = "ami-09558250a3419e7d0"
   tags = {
-    Name = "mike.green example ec2"
-    tag01 = "resource-specific"
+    Name  = "mike.green example ec2"
+    tag01 = "resource-specific-changed-back"
   }
 
 }
+
+
