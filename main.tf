c842da3155cfdef14662c9df963121a6c3dc5fe0
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-11-05 11:12:37 -0600

    fixin'

--- main.tf
+++ main.tf
@@ -30,39 +30,13 @@ provider "aws" {
   }
 }
 
-# resource "aws_instance" "ec2-example" {
-#   instance_type = "t2.micro"
-#   ami           = "ami-09558250a3419e7d0"
-#   tags = {
-#     Name  = "mike.green example ec2"
-#     tag01 = "resource-specific-changed-back"
-#   }
-# }
+resource "aws_instance" "ec2-example" {
+   instance_type = "t2.micro"
+   ami           = "ami-09558250a3419e7d0"
+   tags = {
+     Name  = "mike.green example ec2"
+     tag01 = "resource-specific-changed-back"
+   }
+ }
 
-module "second-service" {
-  source = "./modules/second-service"
-}
 
-output "ss-json" {
-  value = jsonencode(module.second-service.second-service-definition)
-}
-
-locals {
-  defs = [
-    {
-      name      = "first"
-      image     = "service-first"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    },
-    # add additional container from the module
-    module.second-service.second-service-definition
-  ]
-}
